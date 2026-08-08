4ac366f797c53ce96a4e79d9c4cecd04dd5988bb
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-09 00:12:19 +0530

    Update rotate-array.cpp

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // It will be more efficient because it's Time-Complexity is  'O(n)'
+
     void swapping(vector<int> &nums, int i, int j){
         while(i<j){
             swap(nums[i], nums[j]);
@@ -8,7 +10,7 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        k %= nums.size();
+        k %= nums.size();        // If 'k' is greater than 'n' then it will find the less no. of rotations, which will be equal array, when it is rotated 'k' times
         int n = nums.size()-1;
         swapping(nums,0,n);
         swapping(nums,0,k-1);
@@ -16,4 +18,31 @@ public:
 
         for(int ele : nums) cout<<ele<<" ";
     }
+
+
+    //OR
+
+
+    // It will be less efficient because it's Time-Complexity is  'O(k*n)'
+
+    // int temp;
+    // int i = nums.size();
+    // while(k!=0){
+    //     temp = nums[i];
+    //     for(int j=nums.size()-1;j>=1;j--){
+    //         nums[j] = nums[j-1];
+    //     }
+    //     nums[0] = temp;
+    //     k--;
+    // }
+
+    // for(int i=0;i<k/2;i++,k--){
+    //     swap(nums[i],nums[k]);
+    // }
+
+    // for(int ele : nums) cout<<ele<<" ";
+
+    // for(int i=0;i<nums.size();i++){
+    //     cout<<nums[i]<<" ";
+    // }
 };
